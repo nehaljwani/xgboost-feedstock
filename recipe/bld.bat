2d9868ee9f89fa2ad63016725ebc28ce4f16818f
Jonathan J. Helmus <jjhelmus@gmail.com>
2018-06-29 16:11:46 -0500

    disable whole program opt on windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,9 @@
 
+:: py-xgboost has issue if whole program optimization is enabled so remove
+:: it from CFLAGS and CXXFLAGS
+set "CFLAGS= -MD"
+set "CXXFLAGS= -MD"
+
 mkdir build
 cd build
 cmake .. -G"Visual Studio 14 2015 Win64" -DUSE_CUDA=ON
